8021d7b491017d2ccb3d070af005e4914dc90b56
MLyjak1 <mlyjak@gmail.com>
2022-09-05 00:21:58 -0400

    Set up home page and test navbar

--- esports-virtual-leagues/client/src/App.js
+++ esports-virtual-leagues/client/src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import {
+// 	ApolloClient,
+// 	InMemoryCache,
+// 	ApolloProvider,
+// 	createHttpLink,
+// } from "@apollo/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// import './App.css';
+
+import Home from "./pages/Home";
+import Navbar from './components/Navbar';
+
+// const httpLink = createHttpLink({
+//   uri: "/graphql",
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem("id_token");
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+// 	link: authLink.concat(httpLink),
+// 	cache: new InMemoryCache(),
+// });
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <ApolloProvider>
+			<Router>
+		 		<div>
+		 			<Navbar />
+		 		</div>
+				<Routes>
+    <Route path="/" element={<Home />} />
+		 		</Routes>
+			</Router>
+		// </ApolloProvider>
   );
 }
 
 export default App;
+
+
+// <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
---
+++ esports-virtual-leagues/client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <h1> TEST Hello Capoue q</h1>
+    )
+};
+
+export default Home;
